c354bac2d57e10504caf950a01c9d9673dc28877
NurPyth <raznurs@gmail.com>
2025-04-18 17:54:55 +0600

    fix

--- pages/simple_button.py
+++ pages/simple_button.py
@@ -1,7 +1,5 @@
 from pages.base_page import BasePage
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 button_selector = (By.ID, 'submit-id-submit')
 result_selector = (By.ID, 'result-text')
@@ -20,10 +18,7 @@ class SimpleButtonPage(BasePage):
         return self.button().is_displayed()
 
     def click_button(self):
-        wait = WebDriverWait(self.browser, 10)  # Wait up to 10 seconds
-        button = wait.until(EC.element_to_be_clickable(self.button()))
-        # self.browser.execute_script("arguments[0].scrollIntoView(true);", button)
-        return button.click()
+        return self.button().click()
 
     def result(self):
         return self.find(result_selector)
