65d089d9cbcd5bdd9dd9c0f21abaf0702cefa16a
NurPyth <raznurs@gmail.com>
2025-04-18 17:37:33 +0600

    fix

--- conftest.py
+++ conftest.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import pytest
+from selenium.webdriver.support.wait import WebDriverWait
 
 @pytest.fixture()
 def browser():
@@ -8,5 +9,5 @@ def browser():
     options.add_argument('--headless')
     options.add_argument("--no-sandbox")
     chrome_browser = webdriver.Chrome(options=options)
-    chrome_browser.implicitly_wait(3)
+    chrome_browser.implicitly_wait(5)
     return chrome_browser
--- tests/test_button_page.py
+++ tests/test_button_page.py
@@ -1,6 +1,4 @@
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from pages.simple_button import  SimpleButtonPage
 import allure
 
@@ -20,8 +18,6 @@ def test_button1_clicked(browser):
     with allure.step("Open Simple button page"):
         simple_page = SimpleButtonPage(browser)
         simple_page.open()
-        WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.ID, 'submit-id-submit')))
-        browser.get_screenshot_as_file("screenshots/error.png")
     with allure.step("Click the button"):
         simple_page.click_button()
     with allure.step("Check the result"):
