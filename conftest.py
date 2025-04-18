98fc8653556e3c06bc7f5c7338795e662b7604b5
NurPyth <raznurs@gmail.com>
2025-04-18 15:44:05 +0600

    fix

--- conftest.py
+++ conftest.py
@@ -6,6 +6,6 @@ import pytest
 def browser():
     options = Options()
     options.add_argument('--headless')
-    chrome_browser = webdriver.Chrome()
+    chrome_browser = webdriver.Chrome(options=options)
     chrome_browser.implicitly_wait(3)
     return chrome_browser
