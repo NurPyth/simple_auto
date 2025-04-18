3f2c63282794b03b2da510a94ad50e4cf13b13cc
NurPyth <raznurs@gmail.com>
2025-04-18 17:55:42 +0600

    fix

--- conftest.py
+++ conftest.py
@@ -1,13 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import pytest
-from selenium.webdriver.support.wait import WebDriverWait
 
 @pytest.fixture()
 def browser():
     options = Options()
     options.add_argument('--headless')
-    options.add_argument("--no-sandbox")
     chrome_browser = webdriver.Chrome(options=options)
     chrome_browser.maximize_window()
     chrome_browser.implicitly_wait(5)
