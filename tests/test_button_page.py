5945ad3da38ded70f72857ba7fad15c041a5d95a
NurPyth <raznurs@gmail.com>
2025-04-18 16:04:29 +0600

    fix

--- tests/test_button_page.py
+++ tests/test_button_page.py
@@ -1,4 +1,8 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
 from pages.simple_button import  SimpleButtonPage
 
 def test_button1_exist(browser):
@@ -9,5 +13,6 @@ def test_button1_exist(browser):
 def test_button1_clicked(browser):
     simple_page = SimpleButtonPage(browser)
     simple_page.open()
+    WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.ID, 'submit-id-submit')))
     simple_page.click_button()
     assert 'Submitted' == simple_page.result_text
